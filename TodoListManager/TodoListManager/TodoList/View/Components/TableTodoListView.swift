fbb3056c0ec982560201fb45023bf86ba2d7d0f6
Joan Fabregat Bellido <Joan@MacBook-Pro-de-Joan.local>
2017-04-14 12:40:30 +0200

    INS-001: Work in View components. Create View Base for components.

---
+++ TodoListManager/TodoListManager/TodoList/View/Components/TableTodoListView.swift
@@ -0,0 +1,35 @@
+//
+//  TableTodoListView.swift
+//  TodoListManager
+//
+//  Created by Joan Fabregat Bellido on 14/04/2017.
+//  Copyright © 2017 Joan Fabregat Bellido. All rights reserved.
+//
+
+import UIKit
+
+class TableTodoListView: ComponentBaseView, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Cycle life
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        registerNib()
+    }
+    
+    override func initializeSubviews() {
+        let viewName = StyleMSMaintenanceTableView.viewName
+        viewLocal = Bundle.main.loadNibNamed(viewName, owner: self, options: nil)![0] as? UIView
+        viewLocal?.frame = self.bounds
+    }
+    
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        return UITableViewCell
+    }
+}
